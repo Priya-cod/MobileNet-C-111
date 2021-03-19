245550b5d92923b8aff1a3ce72845dfd8b95bf06
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-03-19 17:40:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+Webcam.set({
+    width: 310,
+    height:300,
+    image_format : 'png',
+    png_quality:90,
+
+    constraints: {
+        facingMode: "environment"
+    }
+});
+    camera = document.getElementById("camera");
+
+    Webcam.attach('#camera');
+
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_uri) {
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/b';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+
+function moddelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classifier(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        document.getElementById("object_name").innerHTML = result[0].label;
+    }
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        document.getElementById("object_name").innerHTML = result[0].label;
+    }
+}
